2ed504f222a03ba6e9047ad9737b7a2023d82fea
ragum24 <165797568+ragum24@users.noreply.github.com>
2026-07-28 19:59:51 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -30,3 +30,30 @@ print(array_2)
 
 array_3 = np.arange(18).reshape(2,9)
 print(array_3)
+
+array_4 = np.arange(20).reshape(10,2) 
+print(array_4)
+
+#by without using reshape, the array become a 1D array, while when you 
+#use factors of that number it becomes an 2D array
+
+#generate 10 random numbers and create an array
+
+rand_num = np.random.randint(0,56)
+print(rand_num)
+rand_array = np.random.randint(20,80,10) #parameters: lowest, highest, how many numbers
+print(rand_array)
+
+max_num = rand_array.max()
+print(f"Biggest number: {max_num}")
+
+minnie_mouse = rand_array.min()
+print(f"Smallest number: {minnie_mouse}")
+
+#generating random 2D array
+rand_2D = rand_array.reshape(5,2)
+
+#asending order
+print(f"Sorting a 1D array: {np.sort(rand_array)}")
+print(f"Sorting row-wise: {np.sort(rand_2D,axis=1)}") #axis represents the rows and columns
+
