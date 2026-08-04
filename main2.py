a8ab24f3c846593bd6d3d67d008c1aad01e2708b
ragum24 <165797568+ragum24@users.noreply.github.com>
2026-08-04 20:00:26 +1000

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+a_ray = np.arange(24).reshape(6,4)
+print(f"2D array of 24: \n{a_ray}")
+
+print(f"Dimensions of an array: {a_ray.ndim}")
+
+print(f"Shape (row,column) of an array: {a_ray.shape}")
+
+print(f"Size of an array: {a_ray.size}")
+
